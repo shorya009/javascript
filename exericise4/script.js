c054002304a9cc308e57dcca8520d5b6c5ea096a
Shorya Gupta <61547437+shorya009@users.noreply.github.com>
2020-06-03 17:56:38 +0530

    Update script.js

--- exericise4/script.js
+++ exericise4/script.js
@@ -1,18 +1,18 @@
-function HideshowDisplay(box)
-	{	
-		var ChildBox = document.getElementById(box+'ChildBox');
-		var ParentBox = document.getElementById(box+'ParentBox');
-		if(ParentBox.checked)
-		{
-			ChildBox.style.display = 'block';
-			document.getElementById("container").scrollTop = document.getElementById(box+"Container").offsetTop - 10;
-		}
-		else
-		{
-			ChildBox.style.display = 'none';
-		}
-		var nodes = ChildBox.childNodes;
-		for(i=0; i<nodes.length; i++) {
-			nodes[i].checked = ParentBox.checked;
-		}		
-	}
+function hideShowDisplay(box)
+  { 
+    var childBox = document.getElementById(box+'ChildBox');
+    var parentBox = document.getElementById(box+'ParentBox');
+    if(parentBox.checked)
+    {
+      childBox.style.display = 'block';
+      document.getElementById("container").scrollTop = document.getElementById(box+"Container").offsetTop - 10;
+    }
+    else
+    {
+      childBox.style.display = 'none';
+    }
+    var nodes = childBox.childNodes;
+    for(i=0; i<nodes.length; i++) {
+      nodes[i].checked = parentBox.checked;
+    }   
+  }
