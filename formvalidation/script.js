7581849c58ff5b6c02912767d29fe5b893a1b19d
Shorya Gupta <61547437+shorya009@users.noreply.github.com>
2020-06-11 15:42:37 +0530

    Update script.js

--- formvalidation/script.js
+++ formvalidation/script.js
@@ -1,9 +1,142 @@
-function printError(elemId, hintMsg) {
-  document.getElementById(elemId).innerHTML = hintMsg
+class Formvalidation {
+  constructor(loginid, email, name, timezone, aboutme, checkbox, submit) {
+    this.loginid = loginid
+    this.email = email
+    this.name = name
+    this.timezone = timezone
+    this.aboutme = aboutme
+    this.checkbox = checkbox
+    this.submit = submit
+
+// Defining error variables with a default value
+    this.loginidErr = this.emailErr = this.nameErr = this.aboutmeErr = this.checkboxErr = true
+  }
+
+  printError(elemId, hintMsg) {
+    document.getElementById(elemId).innerHTML = hintMsg
+  }
+
+  validateForm() {
+    
+    let that = this
+
+    // Validate login id
+    if (this.loginid == '') {
+      that.printError('loginidErr', 'Please enter your loginid')
+      loginid.focus()
+      return false
+    } else {
+      let regex = /^[0-9a-zA-Z{2,20}]+$/
+      if (regex.test(this.loginid) === false) {
+        that.printError('loginidErr', 'Please enter a valid login id')
+        loginid.focus()
+        return false
+      } else {
+        that.printError('loginidErr', '')
+        this.loginidErr = false
+      }
+    }
+
+    // Validate email
+    if (this.email == '') {
+      that.printError('emailErr', 'Please enter your email address')
+      email.focus()
+      return false
+    } else {
+      // Regular expression for basic email validation
+      let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+      if (regex.test(this.email) === false) {
+        that.printError('emailErr', 'Please enter a valid email address')
+        email.focus()
+        return false
+      } else {
+        that.printError('emailErr', '')
+        this.emailErr = false
+      }
+    }
+
+    // Validate name
+    if (this.name == '') {
+      that.printError('nameErr', 'Please enter your name')
+      name.focus()
+      return false
+    } else {
+      let regex = /^[a-zA-Z]+$/
+      if (regex.test(this.name) === false) {
+        that.printError('nameErr', 'Please enter a valid name')
+        name.focus()
+        return false
+      } else {
+        that.printError('nameErr', '')
+        this.nameErr = false
+      }
+    }
+    // Validate about me
+    if (this.aboutme == '') {
+      that.printError('aboutmeErr', 'Please enter something about yourself')
+      aboutme.focus()
+      return false
+    } else {
+      let regex = /^.{5,}$/
+      if (regex.test(this.aboutme) === false) {
+        that.printError('aboutmeErr', 'Min 50 characters are required')
+        aboutme.focus()
+        return false
+      } else {
+        that.printError('aboutmeErr', '')
+        this.aboutmeErr = false
+      }
+    }
+
+    if (!this.checkbox) {
+      that.printError('checkboxErr', 'Please check the notification box')
+      return false
+    } else {
+      that.printError('checkboxErr', '')
+      this.checkboxErr = false
+    }
+
+    // Prevent the form from being submitted if there are any errors
+    if (
+      (this.loginidErr ||
+        this.emailErr ||
+        this.nameErr ||
+        this.aboutmeErr ||
+        this.checkboxErr) == true
+    ) {
+      return false
+    } else {
+      // Creating a string from input data for preview
+      this.submit.disabled = true
+      console.log(this.submit.disabled)
+      let dataPreview =
+        "You've entered the following details: \n" +
+        'Login Id: ' +
+        this.loginid +
+        '\n' +
+        'Email Address: ' +
+        this.email +
+        '\n' +
+        'Name: ' +
+        this.name +
+        '\n' +
+        'Timezone: ' +
+        this.timezone +
+        '\n' +
+        'About Me: ' +
+        this.aboutme +
+        '\n'
+
+      // Display input data in a dialog box before submitting the form
+      alert(dataPreview)
+
+      return 1
+    }
+  }
 }
 
-function validateForm() {
-  // Retrieving the values of form elements
+regestrationform.addEventListener('submit', function (e) {
+  e.preventDefault()
   let loginid = document.regestrationform.loginid.value
   let email = document.regestrationform.email.value
   let name = document.regestrationform.name.value
@@ -12,96 +145,18 @@ function validateForm() {
   let checkbox = document.regestrationform.check.checked
   let submit = document.getElementById('submit')
 
-  // Defining error variables with a default value
-  let loginidErr = (emailErr = nameErr = aboutmeErr = checkboxErr = true)
+  let validate = new Formvalidation(
+    loginid,
+    email,
+    name,
+    timezone,
+    aboutme,
+    checkbox,
+    submit
+  )
+  let submitForm = validate.validateForm()
 
-  // Validate login id
-  if (loginid == '') {
-    printError('loginidErr', 'Please enter your loginid')
-  } else {
-    let regex = /^[0-9a-zA-Z]+$/
-    if (regex.test(loginid) === false) {
-      printError('loginidErr', 'Please enter a valid login id')
-    } else {
-      printError('loginidErr', '')
-      loginidErr = false
-    }
+  if (submitForm) {
+    alert('successful submission')
   }
-
-  // Validate email
-  if (email == '') {
-    printError('emailErr', 'Please enter your email address')
-  } else {
-    // Regular expression for basic email validation
-    let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
-    if (regex.test(email) === false) {
-      printError('emailErr', 'Please enter a valid email address')
-    } else {
-      printError('emailErr', '')
-      emailErr = false
-    }
-  }
-
-  // Validate name
-  if (name == '') {
-    printError('nameErr', 'Please enter your name')
-  } else {
-    let regex = /^[a-zA-Z]+$/
-    if (regex.test(name) === false) {
-      printError('nameErr', 'Please enter a valid name')
-    } else {
-      printError('nameErr', '')
-      nameErr = false
-    }
-  }
-  // Validate about me
-  if (aboutme == '') {
-    printError('aboutmeErr', 'Please enter something about yourself')
-  } else {
-    let regex = /^.{50,}$/
-    if (regex.test(aboutme) === false) {
-      printError('aboutmeErr', 'Min 50 characters are required')
-    } else {
-      printError('aboutmeErr', '')
-      aboutmeErr = false
-    }
-  }
-
-  if (!checkbox) {
-    printError('checkboxErr', 'Please check the notification box')
-  } else {
-    printError('checkboxErr', '')
-    checkboxErr = false
-  }
-
-  // Prevent the form from being submitted if there are any errors
-  if (
-    (loginidErr || emailErr || nameErr || aboutmeErr || checkboxErr) == true
-  ) {
-    return false
-  } else {
-    // Creating a string from input data for preview
-    console.log('aaya submit')
-    submit.disabled = true
-    var dataPreview =
-      "You've entered the following details: \n" +
-      'Login Id: ' +
-      loginid +
-      '\n' +
-      'Email Address: ' +
-      email +
-      '\n' +
-      'Name: ' +
-      name +
-      '\n' +
-      'Timezone: ' +
-      timezone +
-      '\n' +
-      'About Me: ' +
-      aboutme +
-      '\n'
-
-    // Display input data in a dialog box before submitting the form
-    alert(dataPreview)
-  }
-}
+})
