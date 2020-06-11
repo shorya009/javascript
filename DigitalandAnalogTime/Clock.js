5997b17a8ddba448e77aea26473f250baf46a22b
Shorya Gupta <61547437+shorya009@users.noreply.github.com>
2020-06-11 13:02:59 +0530

    Update Clock.js

--- DigitalandAnalogTime/Clock.js
+++ DigitalandAnalogTime/Clock.js
@@ -3,57 +3,60 @@ class Clock {
     if (this.constructor == Clock) {
       throw new Error("Abstract classes can't be instantiated.")
     }
-    let time = new Date();
-    this.object = {
+    let time = new Date()
+    this.timeObject = {
       hours: time.getHours() > 12 ? time.getHours() - 12 : time.getHours(),
       minutes: time.getMinutes(),
       seconds: time.getSeconds(),
       amPm: time.getHours() > 12 ? 'PM' : 'AM',
-    };
+    }
   }
 }
 
 class DigitalClock extends Clock {
   getDigitalTime() {
-    let currentTime = this.object.hours + ':' + this.object.minutes + ':' + this.object.seconds + ' ' + this.object.amPm;
-    alert(currentTime);
+    let currentTime =
+      this.timeObject.hours +
+      ':' +
+      this.timeObject.minutes +
+      ':' +
+      this.timeObject.seconds +
+      ' ' +
+      this.timeObject.amPm
+    alert(currentTime)
   }
 }
 class AnalogClock extends Clock {
   getdegree() {
-    let object = {
-      hourDegree: this.object.hours * 30,
-      minuteDegree: this.object.minutes * 6,
-      secondDegree: this.object.seconds * 6,
+    let formatedTime = {
+      hourDegree: this.timeObject.hours * 30,
+      minuteDegree: this.timeObject.minutes * 6,
+      secondDegree: this.timeObject.seconds * 6,
     }
 
-    return object;
+    return formatedTime
   }
   getAnalogTime() {
-    let degree = this.getdegree();
+    let degree = this.getdegree()
 
     alert(` Hour Hand at ${degree.hourDegree}deg
  Minute Hand at ${degree.minuteDegree}deg
- Second Hand at ${degree.secondDegree}deg `);
+ Second Hand at ${degree.secondDegree}deg `)
   }
 }
 
-(function() {
-  let analogTime = new AnalogClock();
-  let digitalTime = new DigitalClock();
-  let getAttribute = document.querySelectorAll('button');
-  let getAttributeArray = Array.from(getAttribute);
+;(function () {
+  let getTimeFormat = document.querySelectorAll('[data-clock]')
 
-  getAttributeArray.forEach(function(element) {
-    element.addEventListener('click', function(e) {
-      if (element.dataset.show == 'true' && element.dataset.id == 'digital') {
-        digitalTime.getDigitalTime();
-      } else if (
-        element.dataset.show == 'true' && element.dataset.id == 'analog') {
-        analogTime.getAnalogTime();
+  getTimeFormat.forEach(function (element) {
+    element.addEventListener('click', function (e) {
+      if (element.dataset.clock == 'digital') {
+        let digitalTime = new DigitalClock()
+        digitalTime.getDigitalTime()
+      } else if (element.dataset.clock == 'analog') {
+        let analogTime = new AnalogClock()
+        analogTime.getAnalogTime()
       }
     })
   })
 })()
-
-
