0602ddabcb54fce0aaf305e6cd6a0a88aed29698
Shorya Gupta <61547437+shorya009@users.noreply.github.com>
2020-06-17 16:40:15 +0530

    Update form_validation.js

--- formvalidation/form_validation.js
+++ formvalidation/form_validation.js
@@ -1,71 +1,53 @@
-class FormValidation {
-    
+const minLength = 5;
+
+const error = {
+    emptyField: 'is empty',
+    minCharacters: `requires minimum ${minLength} characters `
+};
+
+class Form {
+
     constructor() {
-        // Defining error variable
-        this.checkError = 0;
-        this.formElements = {
-            loginid: document.regestrationform.loginid.value,
-            email: document.regestrationform.email.value,
-            fullname: document.regestrationform.fullname.value,
-            timezone: document.regestrationform.timezone.value,
-            aboutme: document.regestrationform.aboutme.value,
-            checkbox: document.regestrationform.check.checked,
-        };
+        this.formElements = document.querySelectorAll('[data-field]');
+    }
+
+    addEventListner() {
+        let that = this;
+        regestrationform.addEventListener('submit', function(e) {
+            e.preventDefault();
+            that.addSubmitListner();
+        });
+    }
+
+    addSubmitListner() {
+        let that = this;
+        that.formElements.forEach(function(field) {
+            if (field.dataset.field == 'aboutme') {
+                that.validateLength(field);
+            } else if (field.dataset.field == 'checkbox') {
+                if (!field.checked) {
+                    alert(`${field.name} ${error['emptyField']}`);
+                }
+            } else {
+                that.validateEmpty(field);
+            }
+        });
     }
     //to check empty or null values
-    isBlank(str) {
-        return !str || /^\s*$/.test(str);
+    validateEmpty(field) {
+        return field.value.trim().length === 0 ? alert(`${field.name} ${error['emptyField']}`) : true;
     }
     //to check string length is greater than 50 or not
-    validateLength(str) {
-        return str.length < 50 ? 1 : 0;
+    validateLength(field) {
+        return field.value.trim().length < 5 ? alert(`${field.name} ${error['minCharacters']}`) : true;
     }
-    //to alert message
-    alertMessage(value) {
-        alert(`${value} cannot be empty`);
-        this.checkError = this.checkError + 1;
-    }
-    // to validate form
-    validateForm() {
-        for (let key in this.formElements) {
-            if (this.isBlank(this.formElements[key])) {
-                this.alertMessage(key);
-            } else {
-                if (key == 'aboutme') {
-                    let checkLength = this.validateLength(this.formElements[key]);
-                    if (checkLength == 1) {
-                        alert(`Length of ${key} should be more than 5`);
-                        this.checkError = this.checkError + 1;
-                    }
-                }
-            }
-        }
-        // Prevent the form from being submitted if there are any errors
-        if (this.checkError > 0) {
-            return false;
-        } else {
-            // to prevent double form submission
-            document.regestrationform.submit.disabled = true;
-            // Creating a string from input data for preview
-            let dataPreview = `You've entered the following details:
-         Login Id: ${this.formElements['loginid']},
-         Email Address: ${this.formElements['email']}, 
-         Name: ${this.formElements['fullname']},
-         Timezone: ${this.formElements['timezone']},
-        About Me: ${this.formElements['aboutme']}`;
-            // Display input data in a dialog box before submitting the form
-            alert(dataPreview);
-            return 1;
-        }
+
+    init() {
+        this.addEventListner();
     }
 }
-regestrationform.addEventListener('submit', function (e) {
-    e.preventDefault();
-    let validate = new FormValidation();
-    let submitForm = validate.validateForm();
-    if (submitForm) {
-        alert('successful submission');
-        document.regestrationform.reset();
-        document.regestrationform.submit.disabled = false;
-    }
-});
+
+(function() {
+    let validate = new Form();
+    validate.init();
+})();
